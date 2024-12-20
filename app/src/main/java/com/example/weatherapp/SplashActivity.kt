ceda702d1e739158668ce8dc5782e6b941ca2056
upendrasingh-63 <adshivam4040@gmail.com>
2024-12-20 12:12:28 +0530

    intial commit

---
+++ app/src/main/java/com/example/weatherapp/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.weatherapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },(3000))
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
