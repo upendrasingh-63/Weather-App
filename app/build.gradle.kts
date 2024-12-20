b941674c56d234481354061ba26d7242242c904d
upendrasingh-63 <adshivam4040@gmail.com>
2024-12-20 17:55:36 +0530

    fix UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.airbnb.android:lottie:6.1.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
 }
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,6 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//5eb1de680cfd09d4ea0228f5f6c00fdf
+//https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=5eb1de680cfd09d4ea0228f5f6c00fdf
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,12 +20,32 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-
+        fetchWeatherData()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun fetchWeatherData() {
+        var retrofit=Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+
+        val response= retrofit.getWeather("delhi","5eb1de680cfd09d4ea0228f5f6c00fdf","metric")
+
+        response.enqueue(object: Callback<ApiData>{
+            override fun onResponse(call: Call<ApiData>, response: Response<ApiData>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<ApiData>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
